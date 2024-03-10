121380c6a7c3b92573578e3ea6594d3b7291e97f
Riskiwahyudi1 <133681297+Riskiwahyudi1@users.noreply.github.com>
2024-03-10 17:29:48 +0700

    1)create MVC Structure(Make images and styles directories in public directory. 2)Setup and imple mentation css framework(Tailwind Css) to  file ListBarang.blade.php

--- laravel/resources/views/listBarang.blade.php
+++ laravel/resources/views/listBarang.blade.php
@@ -1,3 +1,20 @@
-<div>
-    <h1>Halaman List barang</h1>
-</div>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Lapstore | List Barang</title>
+    <link href="{{ asset('styles/styleRiski.css') }}" rel="stylesheet">
+    <link href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" rel="stylesheet">
+</head>
+<body>
+
+    <h1  class="bg-blue-300 p-4 mt-6">List Barang</h1>
+    <div>
+        <img src="{{ asset('images/imgRiski/gambar 1.jpeg') }}" alt="tes" class=" p-4 mt-9">
+        <img src="{{ asset('images/imgRiski/gambar 2.jpeg') }}" alt="">
+    </div>
+
+</body>
+</html>
