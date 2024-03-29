0f8be4742460926a1556108b2df861e1e0e6c4dc
Riskiwahyudi1 <133681297+Riskiwahyudi1@users.noreply.github.com>
2024-03-29 20:37:03 +0700

    instal tailwind css into project

--- laravel/resources/views/listBarang.blade.php
+++ laravel/resources/views/listBarang.blade.php
@@ -5,8 +5,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Lapstore | List Barang</title>
-    <link href="{{ asset('styles/styleRiski.css') }}" rel="stylesheet">
-    <link href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" rel="stylesheet">
+    @vite('resources/css/app.css')
+    @vite('resources/js/app.js')
 </head>
 <body>
 
