2b33a70059f41a96437f4a5933db681e2630adc1
Riskiw <riskiwahyudi1306@gmail.com>
2024-05-19 00:00:38 +0700

    update route and connect source code to database

--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\LoginController;
 use App\Http\Controllers\LandingPageController;
 use App\Http\Controllers\DashboardController;
-use App\Http\Controllers\Home;
+use App\Http\Controllers\HomeController;
 use App\Http\Controllers\InvoiceController;
 use App\Http\Controllers\Detail;
 
@@ -13,6 +13,6 @@ use App\Http\Controllers\Detail;
 Route::get('/login', [LoginController::class, 'login']);
 Route::get('/landingPage', [LandingPageController::class, 'landingPage']);
 Route::get('/dashboard', [DashboardController::class, 'dashboard']);
-Route::get('/home', [Home::class, 'home']);
+Route::get('/home', [HomeController::class, 'home']);
 Route::get('/invoice', [InvoiceController::class, 'invoice']);
 Route::get('/detail', [Detail::class, 'detail']);
