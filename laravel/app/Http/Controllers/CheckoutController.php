44476c8b3e770b67a0d42c254707cfe6deeeb468
Riskiw <riskiwahyudi1306@gmail.com>
2024-05-27 22:08:49 +0700

    menambahkan fitur checkout produk

--- laravel/app/Http/Controllers/CheckoutController.php
+++ laravel/app/Http/Controllers/CheckoutController.php
@@ -8,7 +8,34 @@ class CheckoutController extends Controller
 {
     public function checkout(){
         return view('checkout',[
-            "title" => "Halaman Checkout"
+            'title' => 'Checkout',
+            'active' => 'checkout',
+            'kategoris' => Kategori::all(),
         ]);
+    }
+    public function getProduk(request $request){
+        $productIds = $request->input('check-produk', []);
+        $jumlah = $request->input('jumlah', []);
+        if (!empty($productIds)) {
+            $products = Produk::whereIn('id', $productIds)->get();
+        } else {
+            $products = collect();
+        }
+        $jumlahProduk = [];
+        foreach ($productIds as $productId) {
+            // Cari indeks dari productId dalam array productIds
+            $index = array_search($productId, $request->input('check-produk'));
+            if ($index !== false && isset($jumlah[$index])) {
+                $jumlahProduk[$productId] = $jumlah[$index];
+            } else {
+                $jumlahProduk[$productId] = 1; // atau nilai default lainnya
+            }
+        }
+
+        return view('checkout', [
+            'products' => $products,
+            'jumlah' => $jumlahProduk,
+            'title' => 'Checkout'
+        ]);  
     }
 }
