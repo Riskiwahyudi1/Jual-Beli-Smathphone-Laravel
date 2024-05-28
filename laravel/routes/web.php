0cf8c61ad1a410d5f7602d84ecc43ba743b9a14b
achillsetara16 <150452024+achillsetara16@users.noreply.github.com>
2024-05-28 18:46:58 +0700

    menambahkan keranjangcontroller dan keranjang.blade

--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -7,6 +7,7 @@ use App\Http\Controllers\LoginController;
 use App\Http\Controllers\InvoiceController;
 use App\Http\Controllers\CheckoutController;
 use App\Http\Controllers\HomePenjualController;
+use App\Http\Controllers\KeranjangController;
 
 
 
@@ -17,6 +18,7 @@ Route::get('/home', [HomeController::class, 'home']);
 Route::get('/invoice', [InvoiceController::class, 'invoice']);
 Route::get('/detail/{produk:id}{slug}', [Detail::class, 'detail']);
 Route::get('/checkout', [CheckoutController::class, 'checkout']);
+Route::get('/keranjang', [KeranjangController::class, 'keranjang']);
 
 // route penjual 
 Route::get('/home-penjual', [HomePenjualController::class, 'homePenjual']);
