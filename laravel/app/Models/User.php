ded66f373ec6b5b20a08e1b577f88e3c23a97fea
Riskiw <riskiwahyudi1306@gmail.com>
2024-06-10 20:18:59 +0700

    Update model user

--- laravel/app/Models/User.php
+++ laravel/app/Models/User.php
@@ -19,8 +19,12 @@ class User extends Authenticatable
      */
     protected $fillable = [
         'name',
+        'username',
         'email',
         'password',
+    ];
+    protected $attributes = [
+        'name' => '',
     ];
 
     /**
