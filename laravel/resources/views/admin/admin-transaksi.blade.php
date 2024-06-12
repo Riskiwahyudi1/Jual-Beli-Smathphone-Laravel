d30bddea6e9f8d5f24739e9c7e33d37a57cbceca
Veronikanababan14 <veronikanbbn14@gmail.com>
2024-06-12 21:22:57 +0700

    Update admin-transaksi

--- laravel/resources/views/admin/admin-transaksi.blade.php
+++ laravel/resources/views/admin/admin-transaksi.blade.php
@@ -3,11 +3,18 @@
 <div class="pt-10 ps-40">
     
     <p class="ms-6 mt-12 text-2xl font-bold text-black">Transaksi</p>
-    <input type="search" id="search" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block mx-64 w-2/4 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Cari Transaksi...." required />
-    <button type="submit" style="height: 25px;" class="text-blue-700 absolute   focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"><svg class="w-4 h-4 text-white dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
-                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"/>
-                    </svg></button>
-    <div class="flex justify-around mt-6 mx-12">
+    <div class="flex justify-center mt-4"> <!-- Flexbox container untuk pusat -->
+        <div class="relative w-1/3"> <!-- Kontainer input dan svg -->
+            <input type="text" name="name" class="w-full border h-10 shadow p-4 rounded-xl dark:text-gray-600 dark:border-gray-400 dark:bg-gray-200" placeholder="Cari Produk ...">
+            <svg class="text-gray-600 h-5 w-5 absolute top-2 right-2 fill-current dark:text-gray-100"
+                x="10px" y="10px" viewBox="0 0 56.966 56.966"
+                style="enable-background:new 0 0 56.966 56.966;" xml:space="preserve">
+                <path
+                    d="M55.146,51.887L41.588,37.786c3.486-4.144,5.396-9.358,5.396-14.786c0-12.682-10.318-23-23-23s-23,10.318-23,23  s10.318,23,23,23c4.761,0,9.298-1.436,13.177-4.162l13.661,14.208c0.571,0.593,1.339,0.92,2.162,0.92  c0.779,0,1.518-0.297,2.079-0.837C56.255,54.982,56.293,53.08,55.146,51.887z M23.984,6c9.374,0,17,7.626,17,17s-7.626,17-17,17  s-17-7.626-17-17S14.61,6,23.984,6z">
+                </path>
+            </svg>
+        </div>
+    </div>
         
 
     <table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
@@ -57,10 +64,13 @@
             1.999.000 
             </td>
             <td class="px-6 py-4">
-                Vivo
+                
             </td>
             <td class="px-6 py-4">
-                3      
+                
+            </td>
+            <td class="px-6 py-4">
+                    
             </td>
             <td class="px-6 py-4">   
             <button type="button" class="focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-2 py-2.5 me-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800"></button>
