8e171d8e6d99aa5cc9944c34fa58efc3e4ee2c07
Riskiw <riskiwahyudi1306@gmail.com>
2024-06-13 21:57:22 +0700

    Created fitur checkout and  transaction history

--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -20,19 +20,27 @@ use App\Http\Controllers\TransaksiController;
 
 
 
+Route::get('/', [HomeController::class, 'welcome']);
 Route::get('/register-pembeli', [RegisterPembeliController::class, 'index']);
 Route::post('/register-pembeli', [RegisterPembeliController::class, 'store']);
 Route::get('/login-pembeli', [LoginPembeliController::class, 'login'])->name('login');
 Route::post('/login-pembeli', [LoginPembeliController::class, 'authenticate']);
 Route::post('/logout', [LoginPembeliController::class, 'logout']);
 Route::get('/home', [HomeController::class, 'home']);
-// Route::get('/invoice', [InvoiceController::class, 'invoice']);
-Route::get('/detail-produk/{produk:id}{slug}', [DetailController::class, 'detailProduk']);
-Route::get('/checkout', [CheckoutController::class, 'checkout']);
-Route::get('/keranjang', [KeranjangController::class, 'keranjang']);
+Route::get('/kategori/{kategori:slug}', [HomeController::class, 'kategori']);
+Route::get('/detail-produk/{produk:id}{slug}', [DetailController::class, 'detailProduk'])->middleware('auth');
+Route::get('/brand/{produk:brand}', [HomeController::class, 'brand']);
+Route::get('/keranjang', [KeranjangController::class, 'keranjang'])->middleware('auth');
+Route::post('/hapus-produk', [keranjangController::class, 'hapusProduk']);
 Route::post('/tambah-keranjang', [KeranjangController::class, 'simpanProduk'])->middleware('auth');
-Route::get('/layanan-pengguna', [LayananPenggunaController::class, 'layananPengguna']);
+Route::get('/checkout', [CheckoutController::class, 'checkout']);
+Route::post('/checkout', [CheckoutController::class, 'getProduk']);
+Route::post('/konfirmasi-checkout', [CheckoutController::class, 'konfirmasiCheckout']);
+Route::post('/checkout-dari-detail', [CheckoutController::class, 'getProduk']);
 Route::get('/riwayat-transaksi', [TransaksiController::class, 'riwayatTransaksi']);
+Route::post('/riwayat-transaksi', [TransaksiController::class, 'pembayaran']);
+Route::post('/riwayat-transaksi-batalkan', [TransaksiController::class, 'batalkanTransaksi']);
+
 
 // route penjual 
 Route::get('/home-penjual', [HomePenjualController::class, 'homePenjual']);
