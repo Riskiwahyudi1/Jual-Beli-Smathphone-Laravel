9d4343461b601a9b6f44f39a0aa253dd5c15004b
Veronikanababan14 <veronikanbbn14@gmail.com>
2024-06-14 22:34:03 +0700

    update admin.transaksi

--- laravel/resources/views/admin/admin-transaksi.blade.php
+++ laravel/resources/views/admin/admin-transaksi.blade.php
@@ -66,17 +66,20 @@
             1.999.000 
             </td>
             <td class="px-6 py-4">
+                Veronika
                 
             </td>
             <td class="px-6 py-4">
-                
+                Admin
             </td>
             <td class="px-6 py-4">
-                    
+                    14/06/23
             </td>
             <td class="px-6 py-4">   
-            <button type="button" class="focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-2 py-2.5 me-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800"></button>
-            <button type="button" class="focus:outline-none text-white bg-yellow-400 hover:bg-orange-500 focus:ring-4 focus:ring-orange-300 font-medium rounded-lg text-sm px-2 py-2.5 me-2 mb-2 dark:focus:ring-orange-900"></button>
+            <i class="fas fa-trash mr-2"></i>
+            <button type="button"
+                        class="focus:outline-none text-white bg-yellow-400 hover:bg-yellow-500 focus:ring-4 focus:ring-yellow-300  rounded-full text-md px-1 py-1 me-2 mb-2 dark:focus:ring-yellow-900 font-bold">i</button>
+
 
 
             
