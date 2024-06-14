201f0575a5641ae0ef664d9793fa053ac97bd93c
FarizRizky09 <xfarizrizkyx@gmail.com>
2024-06-14 20:20:26 +0700

    menambahkan login dan register penjual, dan mengedit code yang tersambung ke file tsb

---
+++ database/migrations/2024_04_04_224023_create_produks_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('produks', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('kategori_id');
+            $table->string('nama_produk');
+            $table->text('deskripsi');
+            $table->string('harga');
+            $table-> integer('stok');
+            $table-> integer('terjual');
+            $table->string('brand');
+            $table->string('foto');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('produks');
+    }
+};
