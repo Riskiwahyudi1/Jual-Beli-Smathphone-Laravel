0a776f7d624cd931e2872d30e195ebc71c028b10
sesa12345febri <124112934+sesafebriustrialaka102@users.noreply.github.com>
2024-06-24 22:32:23 +0700

    update back end home admin

--- laravel/app/Http/Controllers/Admin/HomeAdminController.php
+++ laravel/app/Http/Controllers/Admin/HomeAdminController.php
@@ -2,14 +2,25 @@
 
 namespace App\Http\Controllers\Admin;
 
+use App\Models\User;
+use App\Models\Produk;
+use App\Models\Transaksi;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
 class HomeAdminController extends Controller
 {
     public function homeAdmin(){
+        $userPembeli = User::where("role", "buyer")->get();
+        $userPenjual = User::where("role", "seller")->get();
+        $produks = Produk::all();
+        $transaksis = Transaksi::all();
         return view('admin.home-admin', [
-            'title' => 'Home Admin'
+            'title' => 'Home Admin',
+            'totalUserPembeli' => $userPembeli,
+            'totalUserPenjual' => $userPenjual,
+            'produks' => $produks,
+            'transaksis' => $transaksis,
         ]);
         
     }
