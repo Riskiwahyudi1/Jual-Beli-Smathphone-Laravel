0a90d2ff643884209bc52156e1ad12efa2be685c
Riskiw <riskiwahyudi1306@gmail.com>
2024-07-02 18:02:47 +0700

    mengubah tampilan edit produk penjual

--- laravel/resources/views/penjual/edit-produk.blade.php
+++ laravel/resources/views/penjual/edit-produk.blade.php
@@ -68,7 +68,12 @@
             </div>
             <div class="mb-2">
                 <label for="deskripsi-produk" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Deskripsi Produk :</label>
-                <textarea name="deskripsi"  rows="12" class="block p-2.5 w-11/12 text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Masukan Deskripsi Produk" required>{!! nl2br($produk->deskripsi) !!}</textarea>
+                @php
+                $deskripsi = str_replace(['<br>', '<br />'], "\n", $produk->deskripsi);
+            @endphp
+            
+            <textarea name="deskripsi" rows="12" class="block p-2.5 w-11/12 text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Masukan Deskripsi Produk" required>{{ $deskripsi }}</textarea>
+            
             </div>
     </div>
     <div class="w-1/2 mt-10">
